ee86b04c0d99df996573297ed493fa1d95f82734
Adam Pickering <adamkpickering@gmail.com>
2018-03-14 21:06:40 +0000

    Got client.py working

--- client.py
+++ client.py
@@ -6,13 +6,20 @@ from twisted.internet.endpoints import TCP4ClientEndpoint, connectProtocol
 
 
 class Iperf3MuxClient(Protocol):
+
     def connectionMade(self):
-        self.transport.write("SENDPORT\r\n")
+        self.transport.write("SENDPORT\r\n".encode())
 
     def dataReceived(self, data):
         print("Data received: {}".format(data))
 
 
 if __name__ == "__main__":
+    print("getting endpoint...")
     endpoint = TCP4ClientEndpoint(reactor, "192.168.1.101", 10000)
+    print("success")
+    print("getting connection...")
     connection = connectProtocol(endpoint, Iperf3MuxClient())
+    print("success")
+    print("running reactor...")
+    reactor.run()
