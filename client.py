606cc259d78a2f3215b8fb62803fd1833345c227
Adam Pickering <adam@apu2-adam.telus>
2018-03-14 14:31:57 -0600

    Added code for client

--- client.py
+++ client.py
@@ -1,3 +1,18 @@
 #!/usr/bin/env python3
 
-print("here's the client script")
+from twisted.internet import reactor
+from twisted.internet.protocol import Protocol
+from twisted.internet.endpoints import TCP4ClientEndpoint, connectProtocol
+
+
+class Iperf3MuxClient(Protocol):
+    def connectionMade(self):
+        self.transport.write("SENDPORT\r\n")
+
+    def dataReceived(self, data):
+        print("Data received: {}".format(data))
+
+
+if __name__ == "__main__":
+    endpoint = TCP4ClientEndpoint(reactor, "192.168.1.101", 10000)
+    connection = connectProtocol(endpoint, Iperf3MuxClient())
