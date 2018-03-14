0e7999a30330d4b1aa16a36e8777a46f8b32d523
Adam Pickering <adam@apu2-adam.telus>
2018-03-14 13:59:31 -0600

    Wrote initial server code

---
+++ server.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+
+import iperf3
+import threading
+import random
+from twisted.internet.endpoints import TCP4ServerEndpoint
+from twisted.internet.protocol import Factory
+from twisted.protocols.basic import LineReceiver
+from twisted.internet import reactor
+
+
+class Iperf3Mux(LineReceiver):
+
+    def __init__(self, factory):
+        self.factory = factory
+        self.server_thread = None
+        self.server_port = None
+
+    def connectionMade(self):
+        pass
+
+    def connectionLost(self, reason):
+        if (self.server_port is not None) or (self.server_thread is not None):
+            self.stop_server()
+        
+    def lineReceived(self, line):
+        if self.server_port is None:
+            if line == "SENDPORT":
+                self.server_port = random.randrange(10001, 20001)
+                self.run_server(self.server_port)
+                self.sendLine(self.server_port)
+            else:
+                pass
+                # raise error
+                # close connection
+        else:
+            if line == "SENDPORT":
+                self.stop_server()
+                self.server_port = random.randrange(10001, 20001)
+                self.run_server(self.server_port)
+                self.sendLine(self.server_port)
+            else:
+                pass
+                # raise error
+                # close connection
+
+    def run_server(self, port):
+        server = iperf3.Server()
+        server.bind_address = '0.0.0.0'
+        server.port = port
+        self.server_thread = threading.Thread(target=server.run, args=[])
+        self.server_thread.start()
+
+    def stop_server(self):
+        if self.server_thread is not None:
+            if self.server_thread.is_alive():
+                self.server_thread.join()
+            self.server_thread = None
+        if self.server_port is not None:
+            self.server_port = None
+
+
+if __name__ == "__main__":
+    endpoint = TCP4ServerEndpoint(reactor, 10000)
+    endpoint.listen(Factory.forProtocol(Iperf3Mux))
+    #reactor.listenTCP(10000, Factory.forProtocol(Iperf3Mux))
+    reactor.run()
