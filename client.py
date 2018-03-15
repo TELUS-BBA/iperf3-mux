802fd5115a1d46d63fae3c29516e746c1ffab347
Adam Pickering <adamkpickering@gmail.com>
2018-03-15 17:51:35 +0000

    Got iperf3 tests working

--- client.py
+++ client.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import iperf3
 from twisted.internet import reactor
 from twisted.internet.protocol import Protocol
 from twisted.internet.endpoints import TCP4ClientEndpoint, connectProtocol
@@ -7,19 +8,36 @@ from twisted.internet.endpoints import TCP4ClientEndpoint, connectProtocol
 
 class Iperf3MuxClient(Protocol):
 
+    def __init__(self, host):
+        self.test_host = host
+
     def connectionMade(self):
         self.transport.write("SENDPORT\r\n".encode())
 
     def dataReceived(self, data):
-        print("Data received: {}".format(data.decode()))
+        self.test_port = int(data.decode())
+        print("Port received: {}".format(self.test_port))
+        try:
+            result = self.run_test()
+            print("Test result: {}".format(result.sent_Mbps))
+            self.transport.loseConnection()
+        except:
+            self.transport.write("SENDPORT\r\n".encode())
+            return
+
+    def run_test(self):
+        test_client = iperf3.Client()
+        test_client.server_hostname = self.test_host
+        test_client.port = self.test_port
+        result = test_client.run()
+        return result
+
+
+def run_client(host, port):
+    endpoint = TCP4ClientEndpoint(reactor, host, port)
+    connection = connectProtocol(endpoint, Iperf3MuxClient(host))
+    reactor.run()
 
 
 if __name__ == "__main__":
-    print("getting endpoint...")
-    endpoint = TCP4ClientEndpoint(reactor, "192.168.1.101", 10000)
-    print("success")
-    print("getting connection...")
-    connection = connectProtocol(endpoint, Iperf3MuxClient())
-    print("success")
-    print("running reactor...")
-    reactor.run()
+    run_client("192.168.1.101", 10000)
